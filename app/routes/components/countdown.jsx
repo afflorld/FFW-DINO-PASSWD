a7ec345f619c1c05e905b67defec04fe5889df2a
afflorld <sl.adamec@gmail.com>
2023-04-27 23:12:20 +0200

    new file:   app/routes/components/countdown.jsx     modified:   app/styles/app.css 	modified:   app/root.jsx 	modified:   app/routes/index.jsx 	app/routes/components/date.jsx 	app/routes/components/desktop.jsx 	app/routes/components/game.jsx 	app/styles/countdown.css 	app/styles/date.css 	app/styles/desktop.css 	app/styles/dino.css#

---
+++ app/routes/components/countdown.jsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+
+export default function Countdown(){
+
+    useEffect(() => {
+
+            
+        const countDownDate = new Date("May 2, 2023 04:44:00").getTime();
+
+        const interval = setInterval(() => {
+
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("Countdown-top").innerHTML = days + ":";
+        document.getElementById("Countdown-top-half").innerHTML = hours + ":";
+        document.getElementById("Countdown-bottom-half").innerHTML = minutes + ":";
+        document.getElementById("Countdown-bottom").innerHTML = seconds;
+
+        if (distance < 0) {
+            clearInterval(interval);
+            document.getElementById("Countdown-top").innerHTML = "DROP";
+            document.getElementById("Countdown-top-half").innerHTML = "";
+            document.getElementById("Countdown-middle").innerHTML = "IS";
+            document.getElementById("Countdown-bottom-half").innerHTML = "";
+            document.getElementById("Countdown-bottom").innerHTML = "HERE!";
+            document.getElementById("Countdown-between_1").innerHTML = "";
+            document.getElementById("Countdown-between_2").innerHTML = "";
+            document.getElementById("Countdown-between_3").innerHTML = "";
+        }
+        }, 1000);
+
+
+        return () => clearInterval(interval);
+ 
+    });
+
+
+    return(
+
+
+        <div className="countdown-wrapper">
+
+            <p id="Countdown-top"></p>
+
+            <p id="Countdown-top-half"></p>
+
+            <p id="Countdown-middle"></p>
+
+            <p id="Countdown-bottom-half"></p>
+
+            <p id="Countdown-bottom"></p>
+
+        </div>
+
+
+    );
+
+
+}
