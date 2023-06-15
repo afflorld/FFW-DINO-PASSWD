81c580f75de952ee88fc1d8db647f51d7df100f3
afflorld <sl.adamec@gmail.com>
2023-06-15 22:27:18 +0200

    modified:   app/root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -27,7 +27,8 @@ export const links = () => {
 
 export const meta = () => ({
   charset: 'utf-8',
-  viewport: 'width=device-width,initial-scale=1',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+
 });
 
 export async function loader({context}) {
